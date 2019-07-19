6f4132b290221fd96300460552ad0c172247e1e1
Guilherme Chinaglia <quinalia.guilherme@gmail.com>
2019-07-18 23:30:38 -0300

    como implementar gráficos dinâmicos com chart.js - INTRODUÇÃO

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var ctx = document.getElementById("myChart");
+
+var chart = ctx.getContext('2d');
+
+chart.fillStyle = "red";
+chart.fillRect(0,0,100,200);
+
+chart.fillStyle = "blue";
+chart.fillRect(150, 50, 100, 200);
